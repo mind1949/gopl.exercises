2a261b750612a0d3e55fde9727fda2490fe06ff8
mindz <lianjie1949@gmail.com>
2019-05-14 20:50:05 +0800

    complete 1.9

--- ex1.9/main.go
+++ ex1.9/main.go
@@ -5,21 +5,27 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
+
+const defaultschema = "http://"
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "missing url!")
+		fmt.Fprintf(os.Stderr, "missing url!\n")
 		os.Exit(1)
 	}
 
 	for _, url := range os.Args[1:] {
+		if !strings.HasSuffix(url, defaultschema) {
+			url = defaultschema + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 		}
 
-		_, err := io.Copy(os.Stdout, resp.Body)
+		_, err = io.Copy(os.Stdout, strings.NewReader(resp.Status+"\n"))
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %v: %v\n", url, err)
