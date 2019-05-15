ae2307db260c5810f269b0a404659d1e227dfa72
mindz <lianjie1949@gmail.com>
2019-05-15 21:10:14 +0800

    complete ex1.12

---
+++ ex1.12/main.go
@@ -0,0 +1,78 @@
+// LIssajous generate GIF animations of random Lissajous figures.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+import (
+	"log"
+	"net/http"
+	"time"
+	"strconv"
+)
+
+var (
+	palette = []color.Color{color.RGBA{0, 0, 0, 1}, color.RGBA{0, 255, 0, 1}}
+	cycles = 20
+)
+const (
+	blackIndex = 0
+	greenIndex = 1
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8080", nil))
+		return
+	}
+	lissajous(os.Stdout, cycles)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Printf("r.ParseForm(): %v\n", err)
+	}
+
+	if _cycles, ok := r.Form["cycles"]; ok {
+		cycles, _ = strconv.Atoi(_cycles[0])
+	}
+
+	lissajous(w, cycles)
+}
+
+func lissajous(out io.Writer, cycles int) {
+	const (
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+
+}
