e20994cb3525e9d6b69a17e587b82d5b9b46ca12
mindz <lianjie1949@gmail.com>
2019-05-25 20:22:52 +0800

    complete ex3.12

---
+++ ex3.12/anagram_test.go
@@ -0,0 +1,24 @@
+package anagram
+
+import (
+	"testing"
+)
+
+func TestIsAnagram(t *testing.T) {
+	tests := []struct {
+		a, b string
+		want bool
+	}{
+		{"aba", "baa", true},
+		{"aaa", "baa", false}, // same characters but different frequencies
+		{"123456789", "541238769", true},
+		{"13456789", "541238769", false},
+	}
+	for _, test := range tests {
+		got := isAnagram(test.a, test.b)
+		if got != test.want {
+			t.Errorf("isAnagram(%q, %q), got %v, want %v",
+				test.a, test.b, got, test.want)
+		}
+	}
+}
