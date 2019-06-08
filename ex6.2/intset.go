cdcb699d32f21c4a318e8087bc23a47bc14b8700
mindz <lianjie1949@gmail.com>
2019-06-08 21:08:11 +0800

    complete ex6.2

---
+++ ex6.2/intset.go
@@ -0,0 +1,61 @@
+//ex6.2 定义一个变长方法(*IntSet).AddAll(...int), 他允许接受遗传整形之作为参数，　比如s.AddAll(1,2,3)
+package intset
+
+type IntSet struct {
+	words []uint64
+}
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint64(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint64(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) AddAll(nums int) {
+	for _, num := range nums {
+		s.Add(num)
+	}
+}
+
+func (s *IntSet) Len() int {
+	count := 0
+	for _, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint64(x%64)
+	if word > len(s.words) || s.words[word]&(1<<uint(bit)) == 0 {
+		return
+	}
+	s.words[word] &^= 1 << uint(bit)
+}
+
+func (s *IntSet) Clear() {
+	for i := range s.words {
+		s.words[i] = 0
+	}
+}
+
+func (s *IntSet) Copy() *IntSet {
+	new := new(IntSet)
+	new.words = make([]uint64, len(s.words))
+	copy(new.words, s.words) // 若用new.words = s.words就会让两者引用同一个数组了，　这就不算是副本了
+	return new
+}
